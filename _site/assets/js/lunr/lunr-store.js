7fb0301813901a73cc766f7c8e167c0b7d23c762
Sean Daniels <sdaniel5@ncsu.edu>
2021-02-24 19:21:23 -0500

    upd

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Sean Daniels, Bad Coder, Posting a post",
+        "excerpt":"This is my first entry. The purpose of this post is to familiarize myself with applying  Jekyll formatting to markdown file. I plan on using this section to document the projects that I am currently working on.   ","categories": [],
+        "tags": [],
+        "url": "/2020/05/27/sean-daniels-site-launched.html",
+        "teaser": null
+      },]
